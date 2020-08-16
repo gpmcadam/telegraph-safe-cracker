be1a838b094948a5ff275a5dd096407ad48fdce2
Gary McAdam <gary@mcad.am>
2020-08-16 16:46:10 +0100

    fix test desc

--- 2020-08-09/2020-08-09.test-answer.js
+++ 2020-08-09/2020-08-09.test-answer.js
@@ -2,6 +2,6 @@ const solution = require("./");
 
 const [answer] = solution();
 
-test("2020-08-02 answer is 1982", () => {
+test("2020-08-09 answer is 3798", () => {
   expect(answer).toEqual([3, 7, 9, 8]);
 });
